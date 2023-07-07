32931bde444c64f3accf51cbfc4e6e91c8ae7f32
Qusay99 <qusay.hamid9@gmail.com>
2023-07-07 16:50:48 +0200

    added game + protocolfunction

---
+++ genetrischerAlgo.py
@@ -0,0 +1,201 @@
+from random import choice, randint
+from spielUmgebung import *
+from spielUmgebung import *
+import numpy as np
+import pandas as pd
+import csv
+
+input_layer = 7
+hidden_layer1 = 9
+hidden_layer2 = 15
+output_layer = 3
+
+matrix1_form = (hidden_layer1,input_layer)
+matrix2_form = (hidden_layer2,hidden_layer1)
+matrix3_form = (output_layer,hidden_layer2)
+
+df = pd.DataFrame(columns=["generation","gen_highest_Fitness","gen_higest_score"])
+
+# Durchführung der Vorwärtspropagation
+def vorwaertpropagation(X, individuum: np.ndarray) -> np.ndarray:
+    # Erstellung der Gewichtungsmatrixen aus dem individuum-Array
+    M1_ende = matrix1_form[0] * matrix1_form[1]
+    M2_start = M1_ende
+    M2_ende = matrix2_form[0] * matrix2_form[1] + M1_ende
+    M3_start = M2_ende
+    M3_ende = M3_start + matrix3_form[0] * matrix3_form[1]
+
+    matrix1 = individuum[:M1_ende].reshape(matrix1_form)
+    matrix2 = individuum[M2_start:M2_ende].reshape(matrix2_form)
+    matrix3 = individuum[M3_start:M3_ende].reshape(matrix3_form)
+
+    # Berechnung der Zwischenergebnisse und Aktivierungsfunktionen
+    hidden1_input = np.matmul(matrix1, X.T)
+    hidden1_activation = np.tanh(hidden1_input)
+    hidden2_input = np.matmul(matrix2, hidden1_activation)
+    hidden2_activation = np.tanh(hidden2_input)
+    output_input = np.matmul(matrix3, hidden2_activation)
+    output_activation = np.exp(output_input.T) / np.sum(np.exp(output_input.T), axis=1)
+    output_activation.reshape(-1, 1)
+
+    return output_activation
+
+def start_training(anzeige, uhr, gewichte, generation):
+    max_punktzahl = 0
+    durchschnittliche_punktzahl = 0
+    test_spiele = 1
+    punktzahl1 = 0
+    schritte_pro_spiel = 2500
+    punktzahl2 = 0
+
+    for _ in range(test_spiele):
+        schlange_start, schlange_position, apfel_position, punktzahl = startposition()
+
+        anzahl_gleiche_richtung = 0
+        vorherige_richtung = 0
+
+        for _ in range(schritte_pro_spiel):
+            aktueller_richtungsvektor, ist_vorne_blockiert, ist_links_blockiert, ist_rechts_blockiert = blocked_directions(
+                schlange_position)
+            winkel, schlange_richtungsvektor, apfel_richtungsvektor_normalisiert, schlange_richtungsvektor_normalisiert = angle_with_apple(
+                schlange_position, apfel_position)
+            vorhersagen = []
+            vorhergesagte_richtung = np.argmax(np.array(vorwaertpropagation(np.array(
+                [ist_links_blockiert, ist_vorne_blockiert, ist_rechts_blockiert, apfel_richtungsvektor_normalisiert[0],
+                 schlange_richtungsvektor_normalisiert[0], apfel_richtungsvektor_normalisiert[1],
+                 schlange_richtungsvektor_normalisiert[1]]).reshape(-1, 7), gewichte))) - 1
+
+            if vorhergesagte_richtung == vorherige_richtung:
+                anzahl_gleiche_richtung += 1
+            else:
+                anzahl_gleiche_richtung = 0
+                vorherige_richtung = vorhergesagte_richtung
+
+            neue_richtung = np.array(schlange_position[0]) - np.array(schlange_position[1])
+            if vorhergesagte_richtung == -1:
+                neue_richtung = np.array([neue_richtung[1], -neue_richtung[0]])
+            if vorhergesagte_richtung == 1:
+                neue_richtung = np.array([-neue_richtung[1], neue_richtung[0]])
+
+            richtung_taste = generate_button_direction(neue_richtung)
+
+            naechster_schritt = schlange_position[0] + aktueller_richtungsvektor
+            if boundaries_collision(schlange_position[0]) == 1 or collision_with_self(naechster_schritt.tolist(),
+                                                                                               schlange_position) == 1:
+                punktzahl1 += -150
+                break
+
+            else:
+                punktzahl1 += 0
+
+            schlange_position, apfel_position, punktzahl = play_game(schlange_start, schlange_position, apfel_position,
+                                                                       richtung_taste, punktzahl, anzeige, uhr, generation)
+
+            if punktzahl > max_punktzahl:
+                max_punktzahl = punktzahl
+
+            if anzahl_gleiche_richtung > 8 and vorhergesagte_richtung != 0:
+                punktzahl2 -= 1
+            else:
+                punktzahl2 += 2
+    belohnung = punktzahl1 + punktzahl2 + max_punktzahl * 5000
+
+
+    return belohnung, punktzahl
+
+
+def berechne_fitness(werte,generation):
+    # Berechnung des Fitnesswerts durch Spielen eines Spiels mit den gegebenen Gewichten im Chromosom
+    fitness = []
+    punktzahl_liste = []
+    for i in range(werte.shape[0]):
+        fit, punktzahl = start_training(display, clock, werte[i], generation)
+        print(f'Fitnesswert des Chromosoms {str(i)}: ', fit)
+        fitness.append(fit)
+        punktzahl_liste.append(punktzahl)
+    return np.array(fitness), np.array(punktzahl_liste)
+
+
+def eltern_finden(population, fitness, anzahl_eltern):
+    # Auswahl der besten Individuen in der aktuellen Generation als Eltern für die Erzeugung der Nachkommen in der nächsten Generation.
+    eltern = np.empty((anzahl_eltern, population.shape[1]))
+    for eltern_num in range(anzahl_eltern):
+        max_fitness_idx = np.where(fitness == np.max(fitness))
+        max_fitness_idx = max_fitness_idx[0][0]
+        eltern[eltern_num, :] = population[max_fitness_idx, :]
+        fitness[max_fitness_idx] = -99999999
+    return eltern
+
+
+def kreuzung(eltern, nachkommen_groeße):
+    # Erzeugung von Nachkommen für die nächste Generation
+    nachkommen = np.empty(nachkommen_groeße)
+    
+    for k in range(nachkommen_groeße[0]): 
+  
+        while True:
+            eltern1_idx = random.randint(0, eltern.shape[0] - 1)
+            eltern2_idx = random.randint(0, eltern.shape[0] - 1)
+            # Nachkommen erzeugen, wenn die beiden Eltern unterschiedlich sind
+            if eltern1_idx != eltern2_idx:
+                for j in range(nachkommen_groeße[1]):
+                    if random.uniform(0, 1) < 0.5:
+                        nachkommen[k, j] = eltern[eltern1_idx, j]
+                    else:
+                        nachkommen[k, j] = eltern[eltern2_idx, j]
+                break
+    return nachkommen
+
+
+
+def mutation(nachkommen_kreuzung):
+    # Mutation der aus der Kreuzung erzeugten Nachkommen, um Variation in der Population zu erhalten
+    
+    for idx in range(nachkommen_kreuzung.shape[0]):
+        for _ in range(25):
+            i = randint(0, nachkommen_kreuzung.shape[1]-1)
+
+        random_value = np.random.choice(np.arange(-1, 1, step=0.001), size=(1), replace=False)
+        nachkommen_kreuzung[idx, i] = nachkommen_kreuzung[idx, i] + random_value
+
+    return nachkommen_kreuzung
+
+
+# def speichern_in_csv(data, dateiname):
+#     with open(dateiname, mode='w', newline='') as file:
+#         writer = csv.writer(file)
+#         writer.writerows(data)
+
+sol_per_pop = 50
+num_weights = input_layer * hidden_layer1 + hidden_layer1* hidden_layer2 + hidden_layer2*output_layer
+
+# Defining the population size.
+pop_size = (sol_per_pop,num_weights)
+#Creating the initial population.
+new_population = np.random.choice(np.arange(-1,1,step=0.01),size=pop_size,replace=True)
+
+num_generations = 10
+
+num_parents_mating = 12
+for generation in range(num_generations):
+    print('##############        GENERATION ' + str(generation)+ '  ###############' )
+    # Measuring the fitness of each chromosome in the population.
+    fitness, punktzahl = berechne_fitness(new_population, generation)
+    df = df.append({"generation":generation,"gen_highest_Fitness":np.max(fitness),"gen_higest_score": np.max(punktzahl)}, ignore_index=True)
+    print('#######  fittest chromosome in gneneration ' + str(generation) +' is having fitness value:  ', np.max(fitness))
+    # Selecting the best parents in the population for mating.
+    eltern = eltern_finden(new_population, fitness, num_parents_mating)
+
+    # Generating next generation using crossover.
+    nachfolger_crossover = kreuzung(eltern, nachkommen_groeße=(pop_size[0] - eltern.shape[0], num_weights))
+
+    # Adding some variations to the offsrping using mutation.
+    offspring_mutation = mutation(nachfolger_crossover)
+
+    # Creating the new population based on the parents and offspring.
+    new_population[0:eltern.shape[0], :] = eltern
+    new_population[eltern.shape[0]:, :] = offspring_mutation
+
+print("Übersicht der gesammelten Daten:\t", df.info())
+
+df.to_csv('training_protokoll.csv', index=False)
---
+++ spielUmgebung.py
@@ -0,0 +1,196 @@
+import random
+import math
+import numpy as np
+import pygame
+
+# Spielfeldgröße
+fensterbreite = 400
+fensterhohe = 400
+
+
+# Farben
+schwarz = (0, 0, 0)
+weiß = (255, 255, 255)
+gruen = (0, 255, 0)
+rot = (255, 0, 0)
+
+
+'''
+Codierung der Richtungsbefehle:
+
+Nach Links -> button_direction = 0
+Nach Rechts -> button_direction = 1
+Nach unten ->button_direction = 2
+Nach oben -> button_direction = 3
+'''
+
+
+
+pygame.init()
+display=pygame.display.set_mode((fensterbreite,fensterhohe))
+clock=pygame.time.Clock()
+
+
+def schlange(schlangen_position, anzeige):
+    for position in schlangen_position:
+        pygame.draw.rect(anzeige, gruen, pygame.Rect(position[0], position[1], 10, 10))
+
+
+def apfel(apfel_position, anzeige):
+    pygame.draw.rect(anzeige, rot, pygame.Rect(apfel_position[0], apfel_position[1], 10, 10))
+
+
+def startposition():
+    schlangen_start = [100, 100]
+    schlangen_position = [[100, 100], [90, 100], [80, 100]]
+    apfel_position = [random.randrange(1, 40) * 10, random.randrange(1, 40) * 10]
+    punktestand = 0
+
+    return schlangen_start, schlangen_position, apfel_position, punktestand
+
+
+def apple_distance_from_snake(apple_position, snake_position):
+    return np.linalg.norm(np.array(apple_position) - np.array(snake_position[0]))
+
+
+
+def schlange_erzeugen(schlangen_start, schlangen_position, apfel_position, button_richtung, punktestand):
+    if button_richtung == 1:
+        schlangen_start[0] += 10
+    elif button_richtung == 0:
+        schlangen_start[0] -= 10
+    elif button_richtung == 2:
+        schlangen_start[1] += 10
+    else:
+        schlangen_start[1] -= 10
+
+    if schlangen_start == apfel_position:
+        apfel_position, punktestand = apple_collision(apfel_position, punktestand)
+        schlangen_position.insert(0, list(schlangen_start))
+    else:
+        schlangen_position.insert(0, list(schlangen_start))
+        schlangen_position.pop()
+
+    return schlangen_position, apfel_position, punktestand
+
+
+def apple_collision(apple_position, score):
+    apple_position = [random.randrange(1, 40) * 10, random.randrange(1, 40) * 10]
+    score += 1
+    return apple_position, score
+
+
+def boundaries_collision(snake_start):
+    if snake_start[0] >= 400 or snake_start[0] < 0 or snake_start[1] >= 400 or snake_start[1] < 0:
+        return 1
+    else:
+        return 0
+
+
+def collision_with_self(snake_start, snake_position):
+    # snake_start = snake_position[0]
+    return 1 if snake_start in snake_position[1:] else 0
+
+
+def is_direction_blocked(snake_position, current_direction_vector):
+    next_step = snake_position[0] + current_direction_vector
+    snake_start = snake_position[0]
+    if boundaries_collision(next_step) == 1 or collision_with_self(next_step.tolist(), snake_position) == 1:
+        return 1
+    else:
+        return 0
+
+
+def blocked_directions(snake_position):
+    current_direction_vector = np.array(snake_position[0]) - np.array(snake_position[1])
+
+    left_direction_vector = np.array([current_direction_vector[1], -current_direction_vector[0]])
+    right_direction_vector = np.array([-current_direction_vector[1], current_direction_vector[0]])
+
+    is_front_blocked = is_direction_blocked(snake_position, current_direction_vector)
+    is_left_blocked = is_direction_blocked(snake_position, left_direction_vector)
+    is_right_blocked = is_direction_blocked(snake_position, right_direction_vector)
+
+    return current_direction_vector, is_front_blocked, is_left_blocked, is_right_blocked
+
+
+def generate_random_direction(snake_position, angle_with_apple):
+    direction = 0
+    if angle_with_apple > 0:
+        direction = 1
+    elif angle_with_apple < 0:
+        direction = -1
+    else:
+        direction = 0
+
+    return direction_vector(snake_position, angle_with_apple, direction)
+
+
+def direction_vector(snake_position, angle_with_apple, direction):
+    current_direction_vector = np.array(snake_position[0]) - np.array(snake_position[1])
+    left_direction_vector = np.array([current_direction_vector[1], -current_direction_vector[0]])
+    right_direction_vector = np.array([-current_direction_vector[1], current_direction_vector[0]])
+
+    new_direction = current_direction_vector
+
+    if direction == -1:
+        new_direction = left_direction_vector
+    if direction == 1:
+        new_direction = right_direction_vector
+
+    button_direction = generate_button_direction(new_direction)
+
+    return direction, button_direction
+
+
+def play_game(snake_start, snake_position, apple_position, button_direction, score, display, clock, generation):
+    crashed = False
+    while not crashed:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                crashed = True
+        display.fill(schwarz)
+
+        apfel(apple_position, display)
+        schlange(snake_position, display)
+
+        snake_position, apple_position, score = schlange_erzeugen(snake_start, snake_position, apple_position,
+                                                               button_direction, score)
+        pygame.display.set_caption(f"SCORE: {str(score)}; Generation: {str(generation)} ")
+        pygame.display.update()
+        clock.tick(5000)
+
+        return snake_position, apple_position, score
+
+
+def generate_button_direction(new_direction):
+    button_direction = 0
+    if new_direction.tolist() == [10, 0]:
+        return 1
+    elif new_direction.tolist() == [-10, 0]:
+        return 0
+    elif new_direction.tolist() == [0, 10]:
+        return 2
+    else:
+        return 3
+
+def angle_with_apple(snake_position, apple_position):
+    apple_direction_vector = np.array(apple_position) - np.array(snake_position[0])
+    snake_direction_vector = np.array(snake_position[0]) - np.array(snake_position[1])
+
+    norm_of_apple_direction_vector = np.linalg.norm(apple_direction_vector)
+    norm_of_snake_direction_vector = np.linalg.norm(snake_direction_vector)
+    if norm_of_apple_direction_vector == 0:
+        norm_of_apple_direction_vector = 10
+    if norm_of_snake_direction_vector == 0:
+        norm_of_snake_direction_vector = 10
+
+    apple_direction_vector_normalized = apple_direction_vector / norm_of_apple_direction_vector
+    snake_direction_vector_normalized = snake_direction_vector / norm_of_snake_direction_vector
+    angle = math.atan2(
+        apple_direction_vector_normalized[1] * snake_direction_vector_normalized[0] - apple_direction_vector_normalized[
+            0] * snake_direction_vector_normalized[1],
+        apple_direction_vector_normalized[1] * snake_direction_vector_normalized[1] + apple_direction_vector_normalized[
+            0] * snake_direction_vector_normalized[0]) / math.pi
+    return angle, snake_direction_vector, apple_direction_vector_normalized, snake_direction_vector_normalized
+
